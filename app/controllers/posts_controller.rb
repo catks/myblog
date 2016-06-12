374ad5899925c020d89c60abeb0095d74eb6ba95
Carlos Atkinson Ferreira Filho <carlos.atks@gmail.com>
2016-06-12 12:27:06 -0300

    Crud Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,51 @@
 class PostsController < ApplicationController
   def index
-    @content_first = "sucesso infinito"
-    @content_second = "sucesso mais que infinito"
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @category = Category.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post Criado"
+    else
+        render "new"
+    end
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @category = Category.all
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path, notice: "Post Atualizado"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path , notice: "Post deletado"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id, :author_id)
   end
 
 end
