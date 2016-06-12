fe81e5d355b84b38e4e4e01325ddd0cb6b2b1d29
Carlos Atkinson Ferreira Filho <carlos.atks@gmail.com>
2016-06-12 00:49:43 -0300

    Heroku Update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = "sucesso infinito"
+    @content_second = "sucesso mais que infinito"
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
